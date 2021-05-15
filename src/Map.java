e25ca9f40f2c3bbad04283559e485d103ad5d194
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-05-14 21:48:22 -0600

    Interfaz Map Inicial

--- src/Map.java
+++ src/Map.java
@@ -1,2 +1,22 @@
-public interface Map {
+public interface Map<K,V>{
+
+    public void clear();
+
+    public boolean containsKey(K key);
+
+    public boolean containsValue(V value);
+
+    public boolean equals(Object o);
+
+    public V get(K key);
+
+    public boolean isEmpty();
+
+    public V put(K key,V value);
+
+    public V remove(K key);
+
+    public int size();
+
+
 }
--- src/SplayTree.java
+++ src/SplayTree.java
@@ -1,2 +1,42 @@
-public class SplayTree <E extends Comparable<E>> implements Map {
+public class SplayTree <K,V> implements Map<K,V> {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(V value) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
